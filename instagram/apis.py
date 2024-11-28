f84fe1f4d6bfe6bafd0d1dd7da5e5bf8513818d2
Raj Undaviya <undaviyaraj2000@gmail.com>
2024-11-28 18:39:03 +0530

    Changes in authorization code

--- instagram/apis.py
+++ instagram/apis.py
@@ -12,7 +12,7 @@ instaloader_obj = instaloader.Instaloader()
 
 def get_authorization_url(client_id=INSTAGRAM_CLIENT_ID):
     client_id = INSTAGRAM_CLIENT_ID
-    authorization_url = f"https://api.instagram.com/oauth/authorize?client_id={client_id}&redirect_uri={INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code"
+    authorization_url = f"https://api.instagram.com/oauth/authorize?client_id={INSTAGRAM_CLIENT_ID}&redirect_uri={INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code"
     return {
         "authorization_url": authorization_url
     }
